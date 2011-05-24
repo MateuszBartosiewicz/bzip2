7b2fe607a23b4ad4c057804fa2ddf6ff609ebcc7
mjay.francis <mjay.francis@596a7bff-4a59-d6eb-2e58-145ae5ad41d3>
2011-05-24 08:55:44 +0000

    Mark constants as static

--- BZip2Constants.java
+++ BZip2Constants.java
@@ -30,67 +30,66 @@ interface BZip2Constants {
 	/**
 	 * First three bytes of the block header marker
 	 */
-	final int BLOCK_HEADER_MARKER_1 = 0x314159;
+	static final int BLOCK_HEADER_MARKER_1 = 0x314159;
 
 	/**
 	 * Last three bytes of the block header marker
 	 */
-	final int BLOCK_HEADER_MARKER_2 = 0x265359;
+	static final int BLOCK_HEADER_MARKER_2 = 0x265359;
 
 	/**
 	 * Number of symbols decoded after which a new Huffman table is selected
 	 */
-	final int HUFFMAN_GROUP_RUN_LENGTH = 50;
+	static final int HUFFMAN_GROUP_RUN_LENGTH = 50;
 
 	/**
 	 * Maximum possible Huffman alphabet size
 	 */
-	final int HUFFMAN_MAXIMUM_ALPHABET_SIZE = 258;
+	static final int HUFFMAN_MAXIMUM_ALPHABET_SIZE = 258;
 
 	/**
 	 * Maximum possible Huffman code length
 	 */
-	final int HUFFMAN_MAXIMUM_CODE_LENGTH = 20;
+	static final int HUFFMAN_MAXIMUM_CODE_LENGTH = 20;
 
 	/**
 	 * Maximum number of alternative Huffman tables
 	 */
-	final int HUFFMAN_MAXIMUM_TABLES = 6;
+	static final int HUFFMAN_MAXIMUM_TABLES = 6;
 
 	/**
 	 * Maximum possible number of Huffman table selectors
 	 */
-	final int HUFFMAN_MAXIMUM_SELECTORS = ((900000 / HUFFMAN_GROUP_RUN_LENGTH) + 2);
+	static final int HUFFMAN_MAXIMUM_SELECTORS = ((900000 / HUFFMAN_GROUP_RUN_LENGTH) + 2);
 
 	/**
 	 * Huffman symbol used for run-length encoding
 	 */
-	final int HUFFMAN_SYMBOL_RUNA = 0;
+	static final int HUFFMAN_SYMBOL_RUNA = 0;
 
 	/**
 	 * Huffman symbol used for run-length encoding
 	 */
-	final int HUFFMAN_SYMBOL_RUNB = 1;
+	static final int HUFFMAN_SYMBOL_RUNB = 1;
 
 	/**
 	 * First three bytes of the end of stream marker
 	 */
-	final int STREAM_END_MARKER_1 = 0x177245;
+	static final int STREAM_END_MARKER_1 = 0x177245;
 
 	/**
 	 * Last three bytes of the end of stream marker
 	 */
-	final int STREAM_END_MARKER_2 = 0x385090;
+	static final int STREAM_END_MARKER_2 = 0x385090;
 
 	/**
 	 * 'B' 'Z' that marks the start of a BZip2 stream
 	 */
-	final int STREAM_START_MARKER_1 = 0x425a;
+	static final int STREAM_START_MARKER_1 = 0x425a;
 
 	/**
 	 * 'h' that distinguishes BZip from BZip2
 	 */
-	final int STREAM_START_MARKER_2 = 0x68;
-
+	static final int STREAM_START_MARKER_2 = 0x68;
 
 }
